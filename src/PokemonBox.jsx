52957dad927d12b29bdc8ac57cf0084fb2a5c235
xli340 <xli340@aucklanduni.ac.nz>
2023-03-11 11:46:32 +1300

    test

--- src/PokemonBox.jsx
+++ src/PokemonBox.jsx
@@ -1,11 +1,16 @@
 import PokemonTile from "./PokemonTile";
 import styles from "./PokemonBox.module.css";
 
-function PokemonBox({PokemonArray, onRemovePokemon, onChangeNickName}) {
+function PokemonBox({ PokemonArray, onRemovePokemon, onChangeNickName }) {
   return (
     <div className={styles.container}>
       {PokemonArray.map((item, index) => (
-        <PokemonTile key={item.id} Pokemon={item} onRemovePokemon={onRemovePokemon} onChangeNickName={onChangeNickName}/>
+        <PokemonTile
+          key={item.id}
+          Pokemon={item}
+          onRemovePokemon={onRemovePokemon}
+          onChangeNickName={onChangeNickName}
+        />
       ))}
     </div>
   );
